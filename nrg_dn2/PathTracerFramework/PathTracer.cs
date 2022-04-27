96b0f4d161d0803f9f66df9c0017a3a0de04fedd
timkriz <tim.kriznik@gmail.com>
2022-04-27 18:03:59 +0200

    Add simple path tracer algorithm

---
+++ nrg_dn2/PathTracerFramework/PathTracer.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static PathTracer.Samplers;
+
+namespace PathTracer
+{
+    class PathTracer
+    {
+        /// <summary>
+        /// Given Ray r and Scene s, trace the ray over the scene and return the estimated radiance
+        /// </summary>
+        /// <param name="r">Ray direction</param>
+        /// <param name="s">Scene to trace</param>
+        /// <returns>Estimated radiance in the ray direction</returns>
+        public Spectrum Li(Ray r, Scene s)
+        {
+            var L = Spectrum.ZeroSpectrum;
+            /* Implement */
+
+            Spectrum beta = Spectrum.Create(1);
+            int nBounces = 0;
+            int maxNBounces = 10;
+            
+
+            while (nBounces < maxNBounces) {
+
+                (double?, SurfaceInteraction) intersectResult = s.Intersect(r);
+                double? distance = intersectResult.Item1;
+                SurfaceInteraction intersection = intersectResult.Item2;
+
+                // 1. Ray does not intersect with scene
+                if (!distance.HasValue)
+                {
+                    break;
+                }
+
+                Vector3 wOut = -r.d;
+
+                // 2. Ray intersects with light
+                if (intersection.Obj is Light) {
+                    L = beta * intersection.Le(wOut);
+                    break;
+                }
+
+                // 3. Ray intersects with object in the scene but not light
+                Shape shape = intersection.Obj as Shape;
+                (Spectrum f, Vector3 wi, double probabilty, _) = shape.BSDF.Sample_f(wOut, intersection);
+
+                double cosineOfAngle = Vector3.AbsDot(wi, intersection.Normal);
+                beta = beta * f * cosineOfAngle / probabilty;
+
+                Ray wIn = intersection.SpawnRay(wi);
+                r = wIn;
+                nBounces++;
+            }
+
+            return L;
+        }
+
+        public Light getHitLight(Ray r, List<Light> lights) {
+            Light hitLight = null;
+            foreach (Light light in lights) {
+                if (light.Intersect(r).Item1.HasValue) {
+                    return light;
+                }
+            }
+            return hitLight;
+        }
+
+        public Dictionary<Primitive, SurfaceInteraction> getSceneIntersections(Ray r, Scene s) {
+            Dictionary < Primitive, SurfaceInteraction > returnSceneIntersections = new Dictionary<Primitive, SurfaceInteraction >();
+
+            foreach (Primitive primitive in s.Elements)
+            {
+                (double?, SurfaceInteraction) intersection = primitive.Intersect(r);
+
+                if (intersection.Item1 != null)
+                {
+                    returnSceneIntersections.Add(primitive, intersection.Item2);
+                }
+
+            }
+            return returnSceneIntersections;
+        }  
+        public void printVector(Vector3 vector) {
+            Console.WriteLine("[" + vector.x.ToString() + " " + vector.y.ToString() + " " + vector.z.ToString() + "]");
+        }
+
+        public void printScene(Scene s) {
+            //Console.WriteLine(s.Elements.Count);
+            foreach (Primitive primitive in s.Elements) {
+                if (primitive is Shape) {
+                    
+                }
+                Console.WriteLine(primitive.GetType());
+            }
+        }
+
+    }
+}
---
+++ nrg_dn2/PathTracerFramework/Primitives/Sphere.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using MathNet.Numerics.Integration;
+
+namespace PathTracer
+{
+    /// <summary>
+    /// Sphere Shape template class - NOT implemented completely
+    /// </summary>
+    class Sphere : Shape
+    {
+        public double Radius { get; set; }
+        public Sphere(double radius, Transform objectToWorld)
+        {
+            Radius = radius;
+            ObjectToWorld = objectToWorld;
+        }
+
+        /// <summary>
+        /// Ray-Sphere intersection - NOT implemented
+        /// </summary>
+        /// <param name="r">Ray</param>
+        /// <returns>t or null if no hit, point on surface</returns>
+        public override (double?, SurfaceInteraction) Intersect(Ray ray)
+        {
+            Ray r = WorldToObject.Apply(ray);
+
+            // TODO: Compute quadratic sphere coefficients
+
+            // TODO: Initialize _double_ ray coordinate values
+
+            // TODO: Solve quadratic equation for _t_ values
+
+            // TODO: Check quadric shape _t0_ and _t1_ for nearest intersection
+
+            // TODO: Compute sphere hit position and $\phi$
+
+            // TODO: Return shape hit and surface interaction
+
+            // A dummy return example
+            double dummyHit = 0.0;
+            Vector3 dummyVector = new Vector3(0, 0, 0);
+            SurfaceInteraction dummySurfaceInteraction = new SurfaceInteraction(dummyVector, dummyVector, dummyVector, dummyVector, this);
+            return (dummyHit, dummySurfaceInteraction);
+        }
+
+        /// <summary>
+        /// Sample point on sphere in world
+        /// </summary>
+        /// <returns>point in world, pdf of point</returns>
+        public override (SurfaceInteraction, double) Sample()
+        {
+            // TODO: Implement Sphere sampling
+
+            // TODO: Return surface interaction and pdf
+
+            // A dummy return example
+            double dummyPdf = 1.0;
+            Vector3 dummyVector = new Vector3(0, 0, 0);
+            SurfaceInteraction dummySurfaceInteraction = new SurfaceInteraction(dummyVector, dummyVector, dummyVector, dummyVector, this);
+            return (dummySurfaceInteraction, dummyPdf);
+        }
+
+        public override double Area() { return 4 * Math.PI * Radius * Radius; }
+
+        /// <summary>
+        /// Estimates pdf of wi starting from point si
+        /// </summary>
+        /// <param name="si">point on surface that wi starts from</param>
+        /// <param name="wi">wi</param>
+        /// <returns>pdf of wi given this shape</returns>
+        public override double Pdf(SurfaceInteraction si, Vector3 wi)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
